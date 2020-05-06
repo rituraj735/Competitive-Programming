bfa560072df819af8f01bdb3a0770dc5883154bd
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-05-06 21:30:37 +0530

    Update cp_template.cpp

--- cp_template.cpp
+++ cp_template.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 #define ios_base::sync_with_stdio(false);cin.tie(NULL); fast
 #define int t;cin>>t;while(t--) test
 #define MOD 1000000007
@@ -9,3 +8,5 @@ using namespace std;
 #define fdec(i,a,b,dec) for(int i=a;i>=b; i-=dec)
 #define int arr[n] inta(n)
 #define ll arr[n]  lla(n)
+#define cin>>n cin(n)
+#define cout<<n cout(n)
