7c28c3905e844984ec6cca95141bed44f274bfbe
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-06-03 13:18:00 +0530

    Create cp_template#2.cpp

---
+++ cp_template#2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+# define ll  long long  int
+# define ld  long double
+# define pb push_back
+# define pp pop_back
+# define ff first
+# define ss second
+# define mp make_pair
+# define ac accumulate
+# define mod 1000000007
+# define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+# define trace1(x)                cerr << #x << ": " << x << "\n";
+# define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << "\n";
+# define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << "\n";
+# define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << "\n";
