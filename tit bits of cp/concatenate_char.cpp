9e2f4e377cd290046f8f62139f3ebb7cb52f955f
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-06-15 20:43:38 +0530

    Create concatenate_char.cpp

---
+++ tit bits of cp/concatenate_char.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    cin>>str;
+    auto concat= string(1,str[0])+ string(1,str[1]);
+    cout<<concat<<endl;
+    return 0;
+}
