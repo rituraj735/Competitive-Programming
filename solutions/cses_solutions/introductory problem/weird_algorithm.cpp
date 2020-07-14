bc7a0a73ddf7924801612a9ef65a5de64cd38c8d
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-07-14 21:30:43 +0530

    Create weird_algorithm.cpp

---
+++ solutions/cses_solutions/introductory problem/weird_algorithm.cpp
@@ -0,0 +1,18 @@
+// Problem statement @ : https://cses.fi/problemset/result/602418/
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    long long n;
+    cin>>n;
+    while(true){
+        cout<<n<<" ";
+        if(n==1)
+            break;
+        if(n%2==0)
+            n /= 2;
+        else
+            n= 3*n+1;
+    }
+    return 0;
+}
