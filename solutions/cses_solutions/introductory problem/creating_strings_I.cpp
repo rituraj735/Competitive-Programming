17b00d3e3393494d972151d265a2e881054bc7f1
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-07-17 13:43:38 +0530

    Add files via upload

---
+++ solutions/cses_solutions/introductory problem/creating_strings_I.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+# define ll  long long  int
+# define ld  long double
+#define fin0(i,a,b,c) for(int i=a;i<b;i+=c)
+#define fin1(i,a,b,c) for(int i=a;i<=b;i+=c)
+#define fde0(i,a,b,c) for(int i=a;i>b;i-=c)
+#define fde1(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define vi vector<int>
+#define vll vector<ll>
+#define vboo vector<bool>
+#define all(a) a.begin(),a.end()
+# define pb push_back
+# define pp pop_back
+# define ff first
+# define ss second
+# define mp make_pair
+# define ac accumulate
+# define mod 1000000007
+# define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define t     \
+    int t;    \
+    cin >> t; \
+    while (t--)
+# define trace1(x)                cerr << #x << ": " << x << "\n";
+# define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << "\n";
+# define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << "\n";
+# define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << "\n";
+
+set<string> perms;
+
+void permutations(string prefix, string suffix){
+   if(suffix.length()==0){
+      perms.insert(prefix);
+      return;
+   }
+   else{
+      for(int i=0;i<suffix.length();i++){
+        permutations((prefix+suffix[i]), suffix.substr(0,i)+suffix.substr(i+1));
+      }
+   }
+}
+
+int main(){
+  IOS;
+  string str;
+  cin>>str;
+  permutations("",str);
+  cout<<perms.size()<<"\n";
+  for(auto x:perms){
+    cout<<x<<"\n";
+  }
+  return 0;
+}
