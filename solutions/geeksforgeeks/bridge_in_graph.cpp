fe6e4ca547334d8bea14fabc389a1d1fd98a7c3c
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-08-13 20:55:43 +0530

    Create bridge_in_graph.cpp

---
+++ solutions/geeksforgeeks/bridge_in_graph.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isBridge(list<int> adj[], int V, int u, int v);
+
+int main() {
+int T;
+cin >> T;
+while (T--) {
+int s, N;
+cin >> s >> N;
+list<int> *adj = new list<int>[s];
+for (int i = 0; i < N; i++) {
+int u, v;
+cin >> u >> v;
+adj[u].push_back (v);
+adj[v].push_back (u);
+}
+int a, b;
+cin >> a >> b;
+if (isBridge(adj, s, a, b))
+cout << 1 << endl;
+else
+cout << 0 << endl;
+}
+}
+ 
+ 
+void dfs(int s,list<int> adj[],bool vis[],int V){
+    vis[s]=true;
+    for(auto x:adj[s]){
+        if(vis[x])
+          continue;
+        vis[x]=true;
+        dfs(x,adj,vis,V);
+    }
+}
+
+bool isBridge(list<int> adj[], int V, int s, int e) {
+    adj[s].remove(e);
+    adj[e].remove(s);
+    bool vis[V];
+    memset(vis,false,sizeof(vis));
+    for(int i=0;i<V;i++){
+        if(!vis[i])
+           dfs(s,adj,vis,V);
+    }
+    if(!vis[e])
+      return true;
+    return false;
+}
