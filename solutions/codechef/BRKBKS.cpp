3511510802c1a31a16df0bb14a29fee82b8b6427
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-08-30 13:50:15 +0530

    Create BRKBKS.cpp

---
+++ solutions/codechef/BRKBKS.cpp
@@ -0,0 +1,47 @@
+// Problem Statement @: https://www.codechef.com/problems/BRKBKS 
+// Problem Name: Breaking Bricks 
+
+#include <bits/stdc++.h>
+using namespace std;
+# define ll  long long  int
+# define ld  long double
+#define fin0(i,a,b,c) for(int i=a;i<b;i+=c)
+#define fin1(i,a,b,c) for(int i=a;i<=b;i+=c)
+#define fde0(i,a,b,c) for(int i=a;i>b;i-=c)
+#define fde1(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define vi vector<int>
+#define vll vector<ll>
+#define vboo vector<bool>
+#define all(a) a.begin(),a.end()
+# define pb push_back
+# define pp pop_back
+# define ff first
+# define ss second
+# define mp make_pair
+# define ac accumulate
+# define mod 1000000007
+# define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define t     \
+    int t;    \
+    cin >> t; \
+    while (t--)
+# define trace1(x)                cerr << #x << ": " << x << "\n";
+# define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << "\n";
+# define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << "\n";
+# define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << "\n";
+const ll INF= 1e18;
+int main(){
+  IOS;
+  t{
+      int s, w[3];
+      cin>>s>>w[0]>>w[1]>>w[2];
+      if(s>=w[0]+w[1]+w[2]){
+          cout<<"1\n";
+      }
+      else if(s>=w[0]+w[1] || s>=w[2]+w[1])
+        cout<<"2\n";
+      else
+         cout<<"3\n";
+  }
+  return 0;
+}
