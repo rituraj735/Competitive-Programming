8c1326237e296f68e31e1581efd3c917d20dea8f
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-08-30 13:56:06 +0530

    Create CUTBOARD.cpp

---
+++ solutions/codechef/CUTBOARD.cpp
@@ -0,0 +1,41 @@
+// Problem Statement @: https://www.codechef.com/problems/CUTBOARD
+// Problem Name: Cut the Board 
+
+#include <bits/stdc++.h>
+using namespace std;
+# define ll  long long  int
+# define ld  long double
+#define fin0(i,a,b,c) for(int i=a;i<b;i+=c)
+#define fin1(i,a,b,c) for(int i=a;i<=b;i+=c)
+#define fde0(i,a,b,c) for(int i=a;i>b;i-=c)
+#define fde1(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define vi vector<int>
+#define vll vector<ll>
+#define vboo vector<bool>
+#define all(a) a.begin(),a.end()
+# define pb push_back
+# define pp pop_back
+# define ff first
+# define ss second
+# define mp make_pair
+# define ac accumulate
+# define mod 1000000007
+# define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define t     \
+    int t;    \
+    cin >> t; \
+    while (t--)
+# define trace1(x)                cerr << #x << ": " << x << "\n";
+# define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << "\n";
+# define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << "\n";
+# define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << "\n";
+const ll INF= 1e18;
+int main(){
+  IOS;
+  t{
+      int n,m;
+      cin>>n>>m;
+      cout<<(n-1)*(m-1)<<"\n";
+  }
+  return 0;
+}
