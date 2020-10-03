7dd8d1ba4de1eaf793eba8676506e689c3644015
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2020-10-03 17:42:05 +0530

    Create topological_sort_dfs.cpp

---
+++ solutions/geeksforgeeks/topological_sort_dfs.cpp
@@ -0,0 +1,84 @@
+// Problem Statement @: https://practice.geeksforgeeks.org/problems/topological-sort/1
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector <int> topoSort(int N, vector<int> adj[]);
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+bool check(int V, vector <int> &res, vector<int> adj[]) {
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return false;
+        }
+    }
+    return true;
+}
+
+// Driver Code
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        vector <int> res = topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+}// } Driver Code Ends
+
+
+// The Graph structure is as folows
+
+/*  Function which sorts the graph vertices in topological form
+*   N: number of vertices
+*   adj[]: input graph
+*/
+// DFS based Topological Sort implementation
+
+void DFS_Utility(stack<int> &stk, vector<int> adj[], int i, bool visited[]){
+    if(visited[i])
+       return ;
+    visited[i] = true;
+    for(auto x: adj[i]){
+    DFS_Utility(stk,adj,x,visited);
+    }
+    stk.push(i);
+}
+
+vector<int> topoSort(int V, vector<int> adj[]) {
+    bool visited[V];
+    stack<int> stk;
+    vector<int> res;
+    memset(visited, false, sizeof(visited));
+    for(int i= 0 ; i< V; i++){
+        if(visited[i]) 
+            continue;
+        DFS_Utility(stk,adj,i,visited);
+    }
+    while(!stk.empty()){
+        int a = stk.top();
+        res.push_back(a);
+        stk.pop();
+    }
+    return res;
+}
