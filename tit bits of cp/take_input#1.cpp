915022eea1871d01e1f377dc15dda6f5def401b9
rituraj735 <46124473+rituraj735@users.noreply.github.com>
2021-07-02 12:29:23 +0530

    Create take_input#1.cpp

---
+++ tit bits of cp/take_input#1.cpp
@@ -0,0 +1,26 @@
+...
+...
+
+....
+....
+....
+  while(cin>>n>>m, !(n==0 && m==0)){   // takes input till n and m are being entered and neither n nor m is 0
+  int from , to;
+  vii adj(n);
+  for(int i=0; i<m; i++){
+  	cin>>from>>to;
+  	adj[--from].pb(--to);
+  } 
+  topological(adj,n,m);
+  fin0(i,0,n,1){
+  	cout<<++order[i]<<" ";
+  }
+  cout<<"\n";
+  
+  }
+  return 0;
+}
+....
+..
+....
+....
